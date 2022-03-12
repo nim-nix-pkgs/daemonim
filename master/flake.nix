24ffe3b31aec81e2fee92883c7197be8ed6fb8c9
hugosenari <hugosenari@gmail.com>
2022-03-12 02:33:25 -0300

    chore: re index daemonim

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''daemonizer for Unix, Linux and OS X'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-daemonim-master.flake = false;
+  inputs.src-daemonim-master.owner = "bung87";
+  inputs.src-daemonim-master.ref   = "refs/heads/master";
+  inputs.src-daemonim-master.repo  = "daemon";
+  inputs.src-daemonim-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-daemonim-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-daemonim-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
