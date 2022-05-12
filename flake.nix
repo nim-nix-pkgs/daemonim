a14ba1ebc28a0461134597710233124ead8df28a
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:51 -0300

    chore: re index daemonim

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."daemonim-master".dir   = "master";
+  inputs."daemonim-master".owner = "nim-nix-pkgs";
+  inputs."daemonim-master".ref   = "master";
+  inputs."daemonim-master".repo  = "daemonim";
+  inputs."daemonim-master".type  = "github";
+  inputs."daemonim-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."daemonim-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
